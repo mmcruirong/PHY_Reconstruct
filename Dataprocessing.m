2b0659ba0657f09dd865fc87ae2750063f6ed358
Ruirong Chen <ruc28@pitt.edu>
2021-12-21 13:53:57 -0500

    add csi processing

--- Dataprocessing.m
+++ Dataprocessing.m
@@ -1,20 +1,34 @@
 close all
-load('/home/labuser/payload_reconstruction/BPSK_full1/payload_10.mat')
+load('/home/labuser/payload_reconstruction/BPSK/payload_0.mat')
 x = 1:48;
+x_p = 1:40;
 error_table = zeros(48,1);
+error_table_t = zeros(40,1);
 for i = 100:150
     CSI = data_set.CSI{i,1};  
+    Pilot = data_set.Pilots{i,1};  
     Tx_data = data_set.Tx_dec{i,1};
     Rx_data = data_set.Rx_dec{i,1};
     [a,b] = find(Tx_data ~= Rx_data);
     error_sub = mod(a,48);
+    error_t = floor(a/48);
     for j = 1:48
         error_table(j,1) = length(find(error_sub == j));
+    end
+    
+    for k = 1:40
+        error_table_t(k,1) = length(find(error_t == k-1));
     end
     figure(i)
     plot(x,angle(CSI)+20);
     hold on
     bar(x,error_table);
+    
+    figure(2*i)
+    plot(x_p,angle(Pilot)+10);
+    legend('1-12','13-24','25-36','37-48')
+    hold on 
+    bar(x_p,error_table_t);
     %figure(2*i)
    % plot(x,angle(CSI));
 end
