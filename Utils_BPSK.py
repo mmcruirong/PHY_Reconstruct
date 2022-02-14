6f55e059950d651d529676934fc98002bb12f7a1
Ruirong Chen <ruc28@pitt.edu>
2022-02-14 13:57:02 -0500

    Reorder dataset

--- Utils_BPSK.py
+++ Utils_BPSK.py
@@ -198,10 +198,10 @@ def load_processed_dataset(path,path1, shuffle_buffer_size, train_batch_size, te
         #groundtruth_test1 = data['groundtruth_test'].astype(np.float32)
         #label_test1 = data['label_test'].astype(np.float32)
         #label1_test1 = data['label1_test'].astype(np.float32)
-    csi_train1 = csi_train1[20000:95000,:,:,:]
-    pilot_train1 = pilot_train1[20000:95000,:,:,:]
-    csi_test1 = csi_train1[0:15000,:,:,:]    
-    pilot_test1 = pilot_train1[0:15000,:,:,:]  
+    csi_train1 = csi_train1[20000:60000,:,:,:]
+    pilot_train1 = pilot_train1[20000:60000,:,:,:]
+    csi_test1 = csi_train1[0:10000,:,:,:]    
+    pilot_test1 = pilot_train1[0:10000,:,:,:]  
     #print('PHY SHAPE 1= ',csi_test1.shape)
     #print('PHY SHAPE = ',csi_test.shape)
     #csi_test1 = csi_test1[1000:2000,:,:,:]        
@@ -467,4 +467,4 @@ def NN_training(generator, discriminator, data_path, data_path1, logdir):
         #print('Inferencing time for 10k frames:', time.time() - start_time)
 
 if __name__ == "__main__":
-    get_processed_dataset("BPSK_full_legacy")
+    get_processed_dataset("BPSK_full")
